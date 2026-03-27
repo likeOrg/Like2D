0ac89381486361c3b635d5d95b86474f118bc98f
Sam P <ssaammp@gmail.com>
2026-03-27 09:30:05 -0500

    fix starter vite config. Just use the package!

--- examples/starter/vite.config.ts
+++ examples/starter/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 export default defineConfig({
   root: '.',
   build: {
@@ -11,18 +9,6 @@ export default defineConfig({
     emptyOutDir: true,
   },
   publicDir: 'public',
-  resolve: {
-    alias: [
-      {
-        find: /^like$/,
-        replacement: path.resolve(__dirname, '../../like/src/index.ts')
-      },
-      {
-        find: /^like\/internal\/(.*)$/,
-        replacement: path.resolve(__dirname, '../../like/src/core/$1.ts')
-      },
-    ],
-  },
   server: {
     watch: {
       ignored: ['!**/like/**']
