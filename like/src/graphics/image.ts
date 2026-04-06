5271e091f512a60e82a49d69cf82d932c382d446
Sam P <ssaammp@gmail.com>
2026-04-06 17:11:42 -0500

    document instability

--- like/src/graphics/image.ts
+++ like/src/graphics/image.ts
@@ -1,13 +1,14 @@
 import { Vector2 } from "../math";
 
 /**
- * An image that can be drawn using {@link Graphics.draw} 
- * 
+ * An image that can be drawn using {@link Graphics.draw}
+ *
  * Unlike raw HTMLImageElement, there is no need to wait for it to load.
  * If the image isn't loaded, it simply won't draw it at all.
- * 
- * If you're planning on loading many large images, simply preload
+ *
+ * If you're planning on loading many large images, simply load
  * these image handles beforehand so that they're ready.
+ *
  */
 export class ImageHandle {
   readonly path: string;
@@ -44,6 +45,12 @@ export class ImageHandle {
     return [this.element?.width ?? 0, this.element?.height ?? 0];
   }
 
+  /**
+   * Escape hatch:
+   *
+   * Yes, you can get the underlying image.
+   * No, it's not stable in the case we ever switch to webGL.
+   */
   getElement(): HTMLImageElement | null {
     return this.element;
   }
